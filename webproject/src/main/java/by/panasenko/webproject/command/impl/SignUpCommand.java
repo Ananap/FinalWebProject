ce4a72cac4ed2f1a079495c3a70be64c3bc42eff
Natasha <panatva@gmail.com>
2021-06-08 11:00:48 +0300

    update project 1

--- webproject/src/main/java/by/panasenko/webproject/command/impl/SignUpCommand.java
+++ webproject/src/main/java/by/panasenko/webproject/command/impl/SignUpCommand.java
@@ -1,25 +1,26 @@
-package by.panasenko.webproject.controller;
+package by.panasenko.webproject.command.impl;
 
-import by.panasenko.webproject.controller.command.CommandProvider;
-import by.panasenko.webproject.dao.ResultCode;
+import by.panasenko.webproject.command.Command;
+import by.panasenko.webproject.command.PagePath;
+import by.panasenko.webproject.command.Router;
+import by.panasenko.webproject.command.Router.RouterType;
 import by.panasenko.webproject.entity.SignUpData;
 import by.panasenko.webproject.exception.ServiceException;
-import by.panasenko.webproject.service.ServiceProvider;
-import by.panasenko.webproject.service.UserService;
+import by.panasenko.webproject.model.dao.ResultCode;
+import by.panasenko.webproject.model.service.ServiceProvider;
+import by.panasenko.webproject.model.service.UserService;
+import by.panasenko.webproject.util.MailSender;
+import by.panasenko.webproject.util.PasswordEncryptor;
 import org.apache.log4j.Logger;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
-public class SignUpServlet extends HttpServlet {
+public class SignUpCommand implements Command {
 
-    private static final Logger logger = Logger.getLogger(SignUpServlet.class);
+    private static final Logger logger = Logger.getLogger(SignUpCommand.class);
 
     private static final String ERROR_MESSAGE = "Error at SignUpServlet";
-    private static final String ERROR_PAGE_COMMAND = "go_to_error_page_command";
     private static final String ATTRIBUTE_USERNAME = "username";
     private static final String ATTRIBUTE_FIRSTNAME = "firstName";
     private static final String ATTRIBUTE_LASTNAME = "lastName";
@@ -27,22 +28,12 @@ public class SignUpServlet extends HttpServlet {
     private static final String ATTRIBUTE_EMAIL = "email";
     private static final String ATTRIBUTE_PHONE = "phone";
     private static final String ATTRIBUTE_EXCEPTION = "exception";
-    private static final String ATTRIBUTE_MESSAGE = "message";
-    private static final String MESSAGE_DUPLICATE_LOGIN_LOCALE = "login_already_taken";
-    private static final String SIGN_UP_PAGE_URL = "sign_up.jsp";
-    private static final String SUCCESS_PAGE_REDIRECT_URL = "Controller?command=go_to_success_page_command";
+    private static final String ATTRIBUTE_DUPLICATE_EMAIL = "duplicateEmail";
+    private static final String ATTRIBUTE_EMAIL_SENT = "emailSent";
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        process(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        process(req, resp);
-    }
-
-    private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    public Router execute(HttpServletRequest req, HttpServletResponse res) {
+        Router router;
         final ServiceProvider serviceProvider = ServiceProvider.getInstance();
         final UserService userService = serviceProvider.getUserService();
 
@@ -52,7 +43,7 @@ public class SignUpServlet extends HttpServlet {
         final String address = req.getParameter(ATTRIBUTE_ADDRESS);
         final String email = req.getParameter(ATTRIBUTE_EMAIL);
         final String phone = req.getParameter(ATTRIBUTE_PHONE);
-        final String password = userService.generateRandomPassword();
+        final String password = PasswordEncryptor.generateRandomPassword();
 
         SignUpData signUpData = new SignUpData();
 
@@ -65,21 +56,21 @@ public class SignUpServlet extends HttpServlet {
         signUpData.setPhoneNumber(phone);
         try {
             ResultCode resultCode = userService.signUp(signUpData);
-
             switch (resultCode) {
-                case RESULT_ERROR_DUPLICATE_EMAIL:
-                    req.setAttribute(ATTRIBUTE_MESSAGE, MESSAGE_DUPLICATE_LOGIN_LOCALE);
-                    req.getRequestDispatcher(SIGN_UP_PAGE_URL).forward(req, resp);
+                case ERROR_DUPLICATE_EMAIL:
+                    req.setAttribute(ATTRIBUTE_DUPLICATE_EMAIL, true);
                     break;
-                case RESULT_SUCCESS:
-                    resp.sendRedirect(SUCCESS_PAGE_REDIRECT_URL);
+                case SUCCESS:
+                    MailSender.sendEmail(email, MailSender.messageEmailUser(username, password));
+                    req.setAttribute(ATTRIBUTE_EMAIL_SENT, true);
                     break;
             }
-
+            router = new Router(PagePath.GO_TO_LOGIN_PAGE, RouterType.FORWARD);
         } catch (ServiceException e) {
             logger.error(ERROR_MESSAGE, e);
             req.setAttribute(ATTRIBUTE_EXCEPTION, e);
-            CommandProvider.getInstance().getCommand(ERROR_PAGE_COMMAND).execute(req, resp);
+            router = new Router(PagePath.ERROR_PAGE, RouterType.FORWARD);
         }
+        return router;
     }
 }
