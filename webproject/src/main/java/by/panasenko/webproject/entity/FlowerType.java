88c048c7212c21e2b3a0f6cf140c184996f3205a
Natasha <panatva@gmail.com>
2021-06-10 12:40:31 +0300

    update project

--- webproject/src/main/java/by/panasenko/webproject/entity/FlowerType.java
+++ webproject/src/main/java/by/panasenko/webproject/entity/FlowerType.java
@@ -28,4 +28,34 @@ public class FlowerType {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        FlowerType that = (FlowerType) o;
+
+        if (id != that.id) return false;
+        if (category != that.category) return false;
+        return description != null ? description.equals(that.description) : that.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (category != null ? category.hashCode() : 0);
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("FlowerType{");
+        sb.append("id=").append(id);
+        sb.append(", category=").append(category);
+        sb.append(", description='").append(description).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
 }
